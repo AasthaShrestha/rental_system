b4aca5b4e9095c07c11b3210fa06af9abe6288b5
karki <karkisayal7@gmail.com>
2024-11-25 19:17:59 +0545

    merge with backend

--- Frontend/src/pages/Rooms.jsx
+++ Frontend/src/pages/Rooms.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import main1 from "../assets/masterRoom.webp";
 import Flex from "../components/Flex";
 import Cards from "../components/Cards";
 import Footer from "../components/Footer";
-import list from "../list.json";
-import { Link } from "react-router-dom";
-
-function Rooms() {
-  const filterData = list.filter((data) => data.type === "rooms");
 import axios from "axios";
 import { Link } from "react-router-dom";
 
